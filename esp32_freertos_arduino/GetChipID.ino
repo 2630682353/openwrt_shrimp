1ba8902a639c3ad6481e26a819690401bad47fb1
2630682353 <2630682353@github.com>
2023-07-03 17:12:48 +0800

    update

--- esp32_freertos_arduino/GetChipID.ino
+++ esp32_freertos_arduino/GetChipID.ino
@@ -6,6 +6,13 @@
 
 const char* ssid     = "zc_test";
 const char* password = "58285390";
+typedef struct sensor_info_st
+{
+	int report_interval;
+	unsigned char type;
+	unsigned char pin;
+	unsigned char pool_id;
+}sensor_info_t;
 
 const char* host = "192.168.10.103";
 const int httpPort = 80;
@@ -13,6 +20,38 @@ const char* streamId   = "....................";
 const char* privateKey = "....................";
 int wifi_connect_times = 20;
 int http_connect_times = 10;
+int temper_report_interval = 600; //s
+int water_level_report_interval = 600;
+int board_sensor_report_interval = 600;
+int air_pressure_report_interval = 60;
+TimerHandle_t temper_report_timer;
+TimerHandle_t water_level_report_timer;
+TimerHandle_t board_sensor_report_timer;
+TimerHandle_t heart_beat_report_timer;
+
+sensor_info_t *sensor_array = NULL;
+
+
+BaseType_t xTimerChangePeriod( TimerHandle_t xTimer,
+                            TickType_t xNewTimerPeriodInTicks,
+                            TickType_t xTicksToWait );
+
+void pxSoftWaveTimer(TimerHandle_t xTimer); //软件定时器回调函数
+
+SoftWaveTimer1 = xTimerCreate(
+							 "heart_beat_timer",
+							 1000,
+							 pdTRUE,
+							 (void*)1,
+							 heart_beat_report_timer);
+ SoftWaveTimer2 = xTimerCreate(
+							 "board_sensor_report_timer",	//定时器句柄
+							 3000,				//定时器周期
+							 pdTRUE,			//周期/单次定时器
+							 (void*)2,			//定时器ID
+							 board_sensor_report_timer);	//回调函数指针
+
+
 int http_send(String url, String &out)
 {
   Serial.println("int http send");
@@ -86,7 +125,20 @@ void main_task( void * parameter )
     }  
 }
  
-  
+void task3()
+{
+	keyVal = KEY_Scan(0);
+    if(keyVal == KEY0_PRES)         //启动定时器
+    {
+       xTimerStart(heart_beat_report_timer,0);
+       xTimerStart(heart_beat_report_timer,0);
+    }
+    if(keyVal == KEY1_PRES)         //关闭定时器
+    {
+       xTimerStop(heart_beat_report_timer,0);
+       xTimerStop(heart_beat_report_timer,0); 
+    }
+}
  
 void task2( void * parameter)
 {
