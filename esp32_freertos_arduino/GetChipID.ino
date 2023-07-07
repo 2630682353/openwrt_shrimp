7bf88ab770d5b23e8ec75c41eca81a878c59f59c
2630682353 <2630682353@github.com>
2023-07-07 16:59:15 +0800

    update

--- esp32_freertos_arduino/GetChipID.ino
+++ esp32_freertos_arduino/GetChipID.ino
@@ -5,14 +5,25 @@
 #include <WiFi.h>
 
 #define PIN_NUM 40
+#define HEART_BEAT_INTERVAL 10*1000
 const char* ssid     = "zc_test";
 const char* password = "58285390";
 struct list_head my_timer_list;
-char *sensor_table[PIN_NUM];
+sensor_info_t *sensor_table[PIN_NUM];
+enum SENSOR_TYPE
+{
+	SENSOR_TEMPER = 1,
+	SENSOR_FEED,
+	SENSOR_AIR_PRESSURE,
+	SENSOR_WATER_LEVEL,
+	SENSOR_HEART_BEAT = 100,
+};
+typedef int (*timer_func)(void *param);
 
 typedef struct sensor_info_st
 {
 	int report_interval;
+	int id;
 	unsigned char type;
 	unsigned char pin;
 	unsigned char pool_id;
@@ -32,6 +43,7 @@ typedef struct util_timer_st {
 	int pin;
 	int interval;
 	int timer_type;
+	int pool_id;
 	String other_param;
 	struct list_head list;
 }util_timer;
@@ -87,23 +99,40 @@ int del_timer(int type)
 	return 0;
 }
 
-int timer_list_init()
-{
-	pthread_mutex_init(&timer_mutex, NULL);
-	return 0;
-}
-
-util_timer* find_board(int pin)
+util_timer* find_timer(int pin)
 {
 	util_timer *t = NULL;
 	list_for_each_entry(t, &my_timer_list, list) {
-		if (t->, client_mac) == 0) {
-			return p;
+		if (t->pin == pin) {
+			return t;
 		}
 	}
-	return p;
+	return t;
 }
 
+int sensor_temper_func(void *param)
+{
+	Serial.println("int temper func");
+}
+int sensor_heart_beat(void *param)
+{
+	Serial.println("int heart_beat func");
+}
+
+
+timer_func get_timer_func(int sensor_type)
+{
+	switch(sensor_type)
+	{
+		case SENSOR_TEMPER:
+			return sensor_temper_func;
+			break
+		case SENSOR_HEART_BEAT:
+			return sensor_heart_beat;
+			break;
+	}
+	return NULL;
+}
 
 const char* host = "192.168.10.103";
 const int httpPort = 80;
@@ -117,7 +146,7 @@ int board_sensor_report_interval = 600;
 int air_pressure_report_interval = 60;
 
 sensor_info_t *sensor_array = NULL;
-char *dev_mac = "aa:aa:aa:aa:aa:aa"
+char *dev_mac = "aa:aa:aa:aa:aa:aa";
 
 int http_send(String url, String &out)
 {
@@ -135,8 +164,7 @@ int http_send(String url, String &out)
 
   // We now create a URI for the request
   
-  Serial.print("Requesting URL: ");
-  Serial.println(url);
+  Serial.println("Requesting URL: " + url);
   // This will send the request to the server
   client.print(String("GET ") + url + " HTTP/1.1\r\n" +
               "Host: " + host + "\r\n" +
@@ -156,15 +184,34 @@ int http_send(String url, String &out)
       String line = client.readStringUntil('\r');
       out = line;
   }
-
-  Serial.println();
-  Serial.println("closing connection");
+  Serial.println("out:"+out);
   return 0;
+}
+
+int report_sensor_info()
+{
+	String url = "/portal_cgi?opt=query_sensor&client_mac="+dev_mac+"sensor_json=";
+	DynamicJsonDocument json_obj(1024);
+	util_timer *t = NULL;
+	int index = 0;
+	list_for_each_entry(t, &my_timer_list, list) {
+		json_obj["sensor_array"][index]["id"] = t->id;
+		json_obj["sensor_array"][index]["report_interval"] = t->interval;
+		json_obj["sensor_array"][index]["pool_id"] = t->pool_id;
+		json_obj["sensor_array"][index]["sensor_pin"] = t->pin;
+		json_obj["sensor_array"][index]["other_param"] = t->other_param;
+		json_obj["sensor_array"][index]["type"] = t->timer_type;
+	}
+	String json_str;
+	serializeJson(json_obj, json_str);
+	url = url+json_str;
+	String out;
+	http_send(url, out);
 }
 
 int fresh_sensor_info()
 {
-	String url = "/portal_cgi?opt=query_temper&client_mac="+dev_mac;
+	String url = "/portal_cgi?opt=query_sensor&client_mac="+dev_mac;
     String out = "";
 
 	http_send(url, out);
@@ -172,47 +219,75 @@ int fresh_sensor_info()
 	DynamicJsonDocument json_obj(1024);
 	deserializeJson(json_obj, out);
 	serializeJson(json_obj, json_str);
-
+	
+	util_timer *t = NULL;
 	for (int i = 0; i < json_obj["data"].length; i++)
 	{
-		sensor_info_t *sensor_info = malloc(sizeof(sensor_info_t));
-		sensor_info->pin = json_obj["data"][i]["sensor_pin"].toInt();
-		sensor_info->type = json_obj["data"][i]["type"].toInt();
-		sensor_table[sensor_info->pin] = sensor_info;
-		
-	}
-	util_timer *t = NULL;
-	list_for_each_entry(t, &my_timer_list, list) {
-		if (strcmp(p->mac, client_mac) == 0) {
-			if (!list_empty(&p->task_list)) {
-				list_for_each_entry_safe(task, task2, &p->task_list, task_list) {
-					if (strcmp(task->task_name, task_name) == 0 && task->task_id == task_id) {
-						list_del(&task->task_list);
-						free(task);
-						break;
-					}				
-				}
-			}
-			break;
+		int pin = json_obj["data"][i]["sensor_pin"].toInt();
+		t = find_timer(pin);
+		if (t)
+		{
+			t->id = json_obj["data"][i]["id"].toInt();
+			t->timer_type = json_obj["data"][i]["type"].toInt();
+			t->interval = json_obj["data"][i]["report_interval"].toInt();
+			t->other_param = json_obj["data"][i]["other_param"];
+			t->pin = pin;
+		}else
+		{
+			t = malloc(sizeof(util_timer));
+			t->timer_type = json_obj["data"][i]["type"].toInt();
+			t->cb_func = get_timer_func(t->timer_type);
+			t->pin = pin;
+			t->expire = uptime() + 20;
+			t->interval = json_obj["data"][i]["report_interval"].toInt();
+			t->loop = 1;
+			t->other_param = json_obj["data"][i]["other_param"];
+			list_add(&t->list, &my_timer_list);
 		}
 	}
-
-	Serial.println("out:"+ out);
-	vTaskDelay(5000);
+	util_timer *p = NULL;
+	util_timer *n = NULL;
+	list_for_each_entry_safe(p, n, &my_timer_list, list) {
+		if (p->timer_type == SENSOR_HEART_BEAT)
+			continue;
+		int exist = 0;
+		for (int i = 0; i < json_obj["data"].length; i++)
+		{
+			if (p->pin == json_obj["data"][i]["sensor_pin"].toInt())
+			{
+				exist = 1;
+				break;
+			}
+		}
+		if (exist == 0) {
+			list_del(&p->list);
+			Serial.println("del timer:"+ p->pin);
+			free(p);
+		}			
+	}
+	report_sensor_info();
+	return 0;
 }
 
-int get_board_sensor_info()
+int wifi_connect()
 {
-	String sensor_json = "";
-	if (http_send("/portal_cgi?opt=query_sensor&client_mac=all", sensor_json) == 0)
-	{
-		DynamicJsonDocument doc(1024);
-		deserializeJson(doc, sensor_json);
-		int sensor_pin = doc["data"][0]["sensor_pin"];
-		
-	}
+	vTaskDelay(10);
+	Serial.print("Connecting to ");
+    Serial.println(ssid);
+	WiFi.begin(ssid, password);
 
-
+    while (WiFi.status() != WL_CONNECTED) {
+        wifi_connect_times--;
+        vTaskDelay(500);
+        if (wifi_connect_times < 0)
+          return -1;
+        Serial.print(".");
+    }
+	Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+	Serial.println(WiFi.localIP());
+	return 0;
 }
 
 void main_task( void * parameter )
@@ -220,26 +295,19 @@ void main_task( void * parameter )
     vTaskDelay(10);
 	char *msg_buf[16] = {0};
 	msg_queue = xQueueCreate(10, sizeof(msg_item_t));
-    wifi_connect_times = 20;
-    http_connect_times = 10;
-    Serial.print("Connecting to ");
-    Serial.println(ssid);
-    WiFi.begin(ssid, password);
-
-    while (WiFi.status() != WL_CONNECTED) {
-        wifi_connect_times--;
-        vTaskDelay(500);
-        if (wifi_connect_times < 0)
-          return;
-        Serial.print(".");
-    }
-
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-    
-    
+	while(wifi_connect() != 0)
+	{
+	}
+    fresh_sensor_info();
+	util_timer *t = malloc(sizeof(util_timer));
+	t->timer_type = SENSOR_HEART_BEAT;
+	t->cb_func = get_timer_func(t->timer_type);
+	t->pin = 100;
+	t->expire = uptime() + 20;
+	t->interval = HEART_BEAT_INTERVAL;
+	t->loop = 1;
+	t->other_param = "0";
+	list_add(&t->list, &my_timer_list);
 	while (1)
 	{
 		xQueueReceive(msg_queue, msg_buf, 1000);
@@ -276,11 +344,7 @@ void task2( void * parameter)
  
 void setup() {
   Serial.begin(115200);
-  for (int i=0; i< PIN_NUM;i++)
-  {
-	sensor_table[i] = NULL;
-  }
-  
+ 
   xTaskCreate(
                     main_task,          //指定任务函数，也就是上面那个task1函数
                     "TaskMain",        //任务名称
